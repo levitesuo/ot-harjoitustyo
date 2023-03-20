21a9f579ab7672017b60f644e5ba8fe701f99586
levitesuo <leevi.suotula@gmail.com>
2023-03-20 10:39:30 +0200

    Writing and testing

--- Projekti/src/level_objects.py
+++ Projekti/src/level_objects.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import math
 
 class platform:
     def __init__(self, pos :tuple, width):
@@ -25,11 +26,38 @@ class player(character):
         self.__acc = np.array([0, 0])
         self.__vel = np.array([0, 0])
         #Player has position, velocity, acceleration and max_speed
-        self.__max_speed = 1
+        self.__max_speed = 100
+        #self.__sprite = pygame.image.load(".\sprites\player.png")
         
     def __str__(self):
-        return f"{self._pos}, {self.__vel}, {self.__acc}"
+        return f"[{int(self._pos[0])} {int(self._pos[1])}], [{int(self.__vel[0])} {int(self.__vel[1])}], [{int(self.__acc[0])} {int(self.__acc[1])}]"
         
+    def move(self, force :tuple):
+        self.__acc += np.asarray(force)
+        
+    def get_speed(self):
+        return np.linalg.norm(self.__vel)
+        
+    def updateAndShow(self):
+        #xsize and ysize placeholders. To be substituted by the max x and y of the screen
+        xsize = 400
+        ysize = 400
+        
+        #Adding acc to vel and setting acc to 0. bcs physics
+        self.__vel += self.__acc
+        self.__acc = np.array([0, 0])
+        
+        #Check if speed is greater than max speed. If it is setting it to max
+        speed = self.get_speed()
+        if speed > self.__max_speed: self.__vel = self.__vel / speed * self.__max_speed
+        
+        #if the character is trying to go off screen on the x axis. Set the x velocity to -x vel so it bounces of the sides. 
+        new_pos = self._pos + self.__vel
+        if new_pos[0] >= xsize or new_pos[0] < 0: 
+            self.__vel = np.array([self.__vel[0] * -1, self.__vel[1]])
+            new_pos = self._pos + self.__vel
+            
+        self._pos = new_pos
         
 if __name__ == "__main__":
     p = platform((1, 2), 3)
--- Projekti/src/tests/levesClasses_test.py
+++ Projekti/src/tests/levesClasses_test.py
@@ -21,3 +21,30 @@ class TestPlayer(unittest.TestCase):
     
     def test_character_init(self):
         self.assertEqual(str(self.play), "[100 200], [0 0], [0 0]")
+    
+    def test_move_method(self):
+        self.play.move((20, 10))
+        self.assertEqual(str(self.play), "[100 200], [0 0], [20 10]")
+        
+    def test_updateAndShow_method_with_no_special_case(self):
+        self.play.move((20, 10))
+        self.play.updateAndShow()
+        self.assertEqual(str(self.play), "[120 210], [20 10], [0 0]")
+        
+    def test_updateAndShow_method_with_x_out_of_bounds_neg(self):
+        play = level_objects.player((0, 0))
+        play.move((-10, 10))
+        play.updateAndShow()
+        self.assertEqual(str(play), "[10 10], [10 10], [0 0]")
+        
+    def test_updateAndShow_method_with_x_out_of_bounds_pos(self):
+        #max x size = 400
+        play = level_objects.player((399, 0))
+        play.move((10, 10))
+        play.updateAndShow()
+        self.assertEqual(str(play), "[389 10], [-10 10], [0 0]")
+        
+    def test_updateAndShow_method_maxspeed_exeeded(self):
+        self.play.move((1000, 1000))
+        self.play.updateAndShow()
+        self.assertEqual(str(self.play), "[170 270], [70 70], [0 0]")
