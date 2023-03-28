337da86b2ff3c31151eeaaf0f1c9c87d55d536d5
Levitesuo <leevi.suotula@gmail.com>
2023-03-28 13:16:07 +0300

    Collision dedection

--- Projekti/src/game_files/entities/bounding_box.py
+++ Projekti/src/game_files/entities/bounding_box.py
@@ -18,12 +18,12 @@ class BoundingBox:
     def check_for_collision(self, other):
         minx1, miny1, maxx1, maxy1 = self.get_corners()
         minx2, miny2, maxx2, maxy2 = other.get_corners()
-        
+
         if maxx1 >= minx2 and maxx2 >= minx1 and maxy1 >= miny2 and maxy2 >= miny1:
             # Calculate the direction vector from box1 to box2
             dx = min(maxx1, maxx2) - max(minx1, minx2)
             dy = min(maxy1, maxy2) - max(miny1, miny2)
-            
+
             # Check which direction box2 is relative to box1
             if dx < dy:
                 if maxx1 < maxx2:
@@ -35,7 +35,7 @@ class BoundingBox:
                     return vector([0, dy])  # box2 is below box1
                 else:
                     return vector([0, -dy])  # box2 is above box1
-        
+
         else:
             return vector([0, 0])
 
--- Projekti/src/game_files/entities/player.py
+++ Projekti/src/game_files/entities/player.py
@@ -25,23 +25,21 @@ class Player(SpritedObject):
         force_vector = vector([force[0], force[1]])
         self.__acc += force_vector
 
-
     def get_falling_bounding_boxes(self):
-        self.__vel += self.__acc
-        self.__acc = vector([0, 0])
+        vel = self.__acc + self.__vel
 
         # Speed scaling could use its own method
-        speed = np.linalg.norm(self.__vel)
+        speed = np.linalg.norm(vel)
         if speed > self.__max_speed:
-            self.__vel = (
-                vector([self.__vel[0] / speed, self.__vel[1] / speed])
+            self.vel = (
+                vector([vel[0] / speed, vel[1] / speed])
                 * self.__max_speed
             )
-            speed = np.linalg.norm(self.__vel)
+            speed = np.linalg.norm(vel)
 
         falling_boxes = []
 
-        normalised_speed_vector = self.__vel / speed
+        normalised_speed_vector = vel / speed
         iterative_speed_vector = normalised_speed_vector
         i = 1
         while np.linalg.norm(iterative_speed_vector) < speed:
@@ -51,6 +49,9 @@ class Player(SpritedObject):
             self._box.update((pos[0], pos[1] + self.sprite.get_height()))
             falling_boxes.append(copy.copy(self._box))
         return falling_boxes
+    
+    def collision_handler(self, collision):
+        self.__vel -= vector([self.__vel[0] * collision[0], self.__vel[1] * collision[1]] )
 
     def __str__(self):
         return (
